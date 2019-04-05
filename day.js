ba443918d08fde6e556c2c66a22310d8b4720ddb
Mary Dang <mdang@rockefeller.edu>
2019-04-05 09:44:18 -0400

    test update

--- day.js
+++ day.js
@@ -1,8 +1,11 @@
+// Returns the day of the week for the given date
 function dayName(date) {
   const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   return daysOfTheWeek[date.getDay()];
 }
 
-function greeting() {
-	console.log(`Hello, world! Happy ${dayName()}.`);
+// Returns a greeting for the given date
+function greeting(date) {
+	let greet = (`Hello, world! Happy ${dayName(date)}.`);
+	return greet;
 }
--- palindrome.js
+++ palindrome.js
@@ -4,7 +4,24 @@ function reverse(string) {
 }
 
 // Returns true for a palindrome, false otherwise
-function palindrome(string) {
-	let processedContent = string.toLowerCase();
+// function palindrome(string) {
+// 	let processedContent = string.toLowerCase();
+// 	return processedContent === reverse(processedContent);
+// }
+
+// returns an array of the username and domain, for uppercase and lowercase
+// function emailParts(string) {
+// 	let processedContent = string.toLowerCase();
+// 	return processedContent.split("@");
+// }
+
+// defines a phrase object
+function Phrase(content) {
+	this.content = content;
+}
+
+// returns true if the phrase is a palindrome, false otherwise
+this.palindrome = function palindrome() {
+	let processedContent = this.content.toLowerCase();
 	return processedContent === reverse(processedContent);
 }
